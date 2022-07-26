e8aa1d97664e777a53e0b53e48ecb18de63ec17b
Risman04 <risman4205@gmail.com>
2022-07-26 11:32:10 +0700

    test gits

---
+++ soal1/index1.php
@@ -0,0 +1,11 @@
+<?php
+
+$angka = [
+
+    1, 0
+
+];
+
+echo "input : (" . $angka[0] . "," . $angka[1] . "," . $angka[1] . "," . $angka[0] . "," . $angka[1] . "," . $angka[1] . "," . $angka[0] . ")";
+
+?>
---
+++ soal2/index1.php
@@ -0,0 +1,11 @@
+<?php
+
+$input = [
+    
+    "H", "E", "L", "L", "O"
+
+];
+
+echo $input[4] . "," . $input[3] . "," . $input[2] . "," . $input[1] . "," . $input[0];
+
+?>
